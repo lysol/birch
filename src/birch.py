0fffd800c649bae0e726a6a60c7143a8328de15b
Derek Arnold <derek@derekarnold.net>
2019-02-21 00:31:02 -0600

    make game resizable

--- src/birch.py
+++ src/birch.py
@@ -7,24 +7,22 @@ for name in names.values():
 
 pygame.init()
 
-size = width, height = 320, 240
+size = 800, 600
 speed = [2, 2]
 black = 0, 0, 0
-
-screen = pygame.display.set_mode(size)
-
-
-
+screen = pygame.display.set_mode(size, pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
 ballrect = textures["r"].get_rect()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
+        if event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode(event.dict['size'], pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+            size = event.dict['size']
     ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    if ballrect.left < 0 or ballrect.right > size[0]:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if ballrect.top < 0 or ballrect.bottom > size[1]:
         speed[1] = -speed[1]
 
     screen.fill(black)
