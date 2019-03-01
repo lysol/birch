2f25d4218d5188bb4c673c6bdfaf8479341ab316
Derek Arnold <derek@derekarnold.net>
2019-03-01 08:58:00 -0600

    adjust toolbox to use variable-sized textures

--- src/toolbox.py
+++ src/toolbox.py
@@ -8,19 +8,39 @@ class Toolbox:
 
     position = 20, 20
     padding = 4
-    icon_spacing = 36
+    icon_spacing = 4
+    width = 77
+    height = 300
 
     def __init__(self, tools, textures):
         self.tools = tools
+        self.tool_rects = []
         self.selected = 0
         self.textures = textures
         self.surface = Surface(self.get_rect()[2:])
+        # build areas for tools
+        for i, tool in enumerate(self.tools):
+            tex = self.textures[tool]
+            size = tex.get_size()
+            if i == 0:
+                left = self.padding
+                top = self.padding
+            else:
+                prev_rect = self.tool_rects[i - 1]
+                top = prev_rect.top
+                left = self.icon_spacing + prev_rect.left + prev_rect.width
+                if left + size[0] > self.width - self.icon_spacing:
+                    left = self.padding
+                    top = prev_rect.top + prev_rect.height + self.icon_spacing
+            r = Rect(left, top, size[0], size[1])
+            self.tool_rects.append(r)
 
     def get_rect(self):
         bounds = (
             self.position[0], self.position[1],
-            self.padding + self.icon_spacing * 2,
-            self.padding + math.ceil(len(self.tools) / 2.0) * self.icon_spacing)
+            self.width,
+            self.height
+            )
         return bounds
 
     def in_bounds(self, pos):
@@ -29,9 +49,14 @@ class Toolbox:
             pos[1] >= bounds[1] and pos[1] <= bounds[1] + bounds[3]
 
     def hover_icon(self, pos):
-        x = math.floor((pos[0] - self.padding - self.position[0]) / self.icon_spacing)
-        y = math.floor((pos[1] - self.padding - self.position[1]) / self.icon_spacing)
-        return x + y * 2
+        p = [
+            pos[0] - self.position[0],
+            pos[1] - self.position[1]
+            ]
+        for i, rect in enumerate(self.tool_rects):
+            if rect.collidepoint(p):
+                return i
+        return None
 
     def draw(self, screen):
         bounds = list(self.get_rect())
@@ -42,11 +67,7 @@ class Toolbox:
         draw.rect(self.surface, BLACK, Rect(0, 0, bounds[2] - 1, bounds[3] - 1), 2)
 
         for i, tool in enumerate(self.tools):
-            icon_position = (
-                (i % 2 * self.icon_spacing) + self.padding,
-                math.floor(i / 2.0) * self.icon_spacing + self.padding
-                )
-            self.surface.blit(self.textures[tool], icon_position)
+            self.surface.blit(self.textures[tool], self.tool_rects[i])
             if i == self.selected:
-                self.surface.blit(self.textures["cursor"], icon_position)
+                self.surface.blit(self.textures["cursor"], self.tool_rects[i])
         return screen.blit(self.surface, self.position)
