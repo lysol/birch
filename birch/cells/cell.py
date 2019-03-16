d9108b44fbca99ead79edb6336a242b3126dcbc7
Derek Arnold <derek@derekarnold.net>
2019-03-16 16:32:56 -0500

    allow tileable cells

--- birch/cells/cell.py
+++ birch/cells/cell.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
-from pygame import Rect, draw
+import pygame
+from pygame import Rect, draw, Surface
 from birch.util import negate, BG_COLOR
 from birch.cells import *
 
@@ -18,23 +19,60 @@ class Cell:
         self.id = uuid4()
         self.priority = priority
         # use the texture to get the size if it is None
+        texture_size = self.textures[self.texture_name].get_size()
         if size is not None:
             self.size = size
         else:
-            self.size = self.textures[self.texture_name].get_size()
+            self.size = texture_size
         self._rect = Rect(
             self.position[0],
             self.position[1],
             self.size[0],
             self.size[1]
             )
+        self._last_texture = None
+        self._texture = None
+        self.dirty_texture = False
+        self._init_texture()
+
+    def _init_texture(self):
+        if self._texture is None or self._last_texture is None or \
+            self._last_texture != self.texture_name:
+            texture_size = self.textures[self.texture_name].get_size()
+            if texture_size[0] < self.size[0] or texture_size[1] < self.size[1]:
+                # need to tile
+                try:
+                    self._texture = Surface(self.size)
+                except pygame.error as e:
+                    print('oops lol', self.size)
+                    raise e
+                self._texture.fill(BG_COLOR)
+                plot = [0, 0]
+                while plot[0] < self.size[0]:
+                    while plot[1] < self.size[1]:
+                        self._texture.blit(self.textures[self.texture_name],
+                            Rect(plot[0], plot[1], texture_size[0], texture_size[1]))
+                        plot[1] += texture_size[1]
+                    plot[0] += texture_size[0]
+                    plot[1] = 0
+                print('texture was tiled')
+            else:
+                self._texture = self.textures[self.texture_name]
+            self._last_texture = self.texture_name
+            self.dirty_texture = False
+
+    @property
+    def texture(self):
+        if self.dirty_texture:
+            self._init_texture()
+        return self._texture
 
     def __str__(self):
         return str(self.__class__) + ": " + str(self.__dict__)
 
     def draw(self, camera, screen):
         coords = self.get_rect(camera)
-        return screen.blit(self.textures[self.texture_name], coords)
+        return screen.blit(self.texture, coords)
 
     def draw_box(self, camera, screen, color):
         coords = self.get_rect(camera)
