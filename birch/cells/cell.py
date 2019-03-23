24f040a165c3152848308279e9a7c98c24159e64
Derek Arnold <derek@derekarnold.net>
2019-03-23 11:31:19 -0500

    fix cell intersection lol

--- birch/cells/cell.py
+++ birch/cells/cell.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 from pyglet import sprite
-from birch.util import negate, BG_COLOR
+from birch.util import negate, BG_COLOR, Rect
 from birch.cells import *
 
 class Cell(sprite.Sprite):
@@ -20,10 +20,21 @@ class Cell(sprite.Sprite):
         self.scale = 2.0
 
     def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
+        return '%s: %s' % (self.__class__, str(self.bounds))
+
+    def __repr__(self):
+        return self.__str__()
 
     def _shift(self, delta):
         return self.position[0] + delta[0], self.position[1] + delta[1]
+
+    @property
+    def w(self):
+        return self.width
+
+    @property
+    def h(self):
+        return self.height
 
     @property
     def left(self):
@@ -57,22 +68,35 @@ class Cell(sprite.Sprite):
     def bottomright(self):
         return self._shift((self.width, self.height))
 
-    def intersects(self, x, y, w=None, h=None):
-        if w is None or h is None:
-            points = [(x, y)]
-        else:
-            r = x + w - 1
-            b = y + h - 1
-            points = ((x, y),
-                (r, y),
-                (r, b),
-                (x, b))
-        for a, b in points:
-            if a >= self.x and b >= self.y and \
-                a < self.x + self.width and \
-                b < self.y + self.height:
-                return True
-        return False
+    @property
+    def rect(self):
+        return Rect(self.x, self.y, self.w, self.h)
+
+    @property
+    def bounds(self):
+        return self.rect.bounds
+
+    def collidepoint(self, x, y):
+        return x >= self.left and x < self.right and \
+                y >= self.top and y < self.bottom
+
+    def intersects(self, x, y=None, w=None, h=None):
+        if issubclass(type(x), Cell):
+            sprite = x
+            x = sprite.x
+            y = sprite.y
+            w = sprite.w
+            h = sprite.h
+        l = x
+        t = y
+        b = y + h - 1
+        r = x + w - 1
+        return not (
+            r < self.left or \
+            l > self.right - 1 or \
+            b < self.top or \
+            t > self.bottom - 1
+            )
 
     def viewed_position(self, camera):
         return self._shift(negate(camera))
