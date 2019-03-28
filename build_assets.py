75d818c065f8a31241ecf39cafdbc0b78d3e68a1
Derek Arnold <derek@derekarnold.net>
2019-03-28 18:13:06 -0500

    allow arbitrary dimensions in names.json

--- build_assets.py
+++ build_assets.py
@@ -8,10 +8,12 @@ for name in data:
     print('Saving %s' % name)
     position = data[name]['position']
     size = data[name]['size']
+    if type(size) == int:
+        size = (size, size)
     region = (
         position[0],
         position[1],
-        position[0] + size,
-        position[1] + size
+        position[0] + size[0],
+        position[1] + size[1]
         )
     im.crop(region).save('birch/assets/%s.png' % name)
