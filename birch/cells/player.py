50e93955b8adfa392bc6707eb3462734af23dfe0
Derek Arnold <derek@derekarnold.net>
2019-05-21 21:17:28 -0500

    use a simpler approach for setting image direction

--- birch/cells/player.py
+++ birch/cells/player.py
@@ -6,6 +6,7 @@ class Player(Cell):
             size=None):
         self.texture_prefix = texture_prefix
         self.direction = '_d'
+        self.texture_name = texture_prefix + self.direction
         super().__init__(name, textures, position, self.texture_name, batch=batch,
             size=size)
         self.speed = 2
@@ -14,18 +15,22 @@ class Player(Cell):
     def go_up(self):
         self.velocity = (self.velocity[0], self.speed)
         self.direction = '_u'
+        self.set_image()
 
     def go_down(self):
         self.velocity = (self.velocity[0], -self.speed)
         self.direction = '_d'
+        self.set_image()
 
     def go_left(self):
         self.velocity = (-self.speed, self.velocity[1])
         self.direction = '_l'
+        self.set_image()
 
     def go_right(self):
         self.velocity = (self.speed, self.velocity[1])
         self.direction = '_r'
+        self.set_image()
 
     def apply_movement(self):
         self.position = (
@@ -34,11 +39,7 @@ class Player(Cell):
             )
         self.velocity = (0, 0)
 
-    @property
-    def texture_name(self):
-        return '%s%s' % (self.texture_prefix, self.direction)
-
-    @texture_name.setter
-    def texture_name(self, name):
-        self.texture_prefix = name
+    def set_image(self):
+        self.texture_name = self.texture_prefix + self.direction
+        self.image = self.textures[self.texture_name]
 
