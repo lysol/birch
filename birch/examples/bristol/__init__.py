757a112104b332e56d6003266181003ebf0a0c17
Derek Arnold <derek@derekarnold.net>
2019-05-24 00:19:54 -0500

    seed some short grass textures as a test

--- birch/examples/bristol/__init__.py
+++ birch/examples/bristol/__init__.py
@@ -1,6 +1,21 @@
 from birch.game import BirchGame
+from random import shuffle, random
+from birch._birch import Perlin
+from birch.cells.blueprint import BlueprintCell
+from birch.examples.bristol.cells.grass import ShortGrass
 
 class Bristol:
+
+    seed_config = {
+        'short_grass': {
+            'class': ShortGrass,
+            'freq': 1/400,
+            'octaves': 1,
+            'offset': [110, -43],
+            'threshold': 0.5,
+            'rand_thresh': 0.95
+            },
+        }
 
     @property
     def state(self):
@@ -32,15 +47,39 @@ class Bristol:
 
     def __init__(self):
         self.ticks = 0
+        self.perlin = Perlin(999)
         self.game = BirchGame('birch/examples/bristol/assets')
         self.game.register_tick_handler(self.tick_handler)
-        #self.game.register_seed_handler(self.seed_handler)
+        self.game.register_seed_handler(self.seed_handler)
         #self.game.register_mouse_handler(self.mouse_handler)
         #self.game.register_mouse_press_handler(self.mouse_press_handler)
         #self.game.register_map_click_handler(self.map_click_handler)
         self.game.camera_controlled = False
         self.game.player_controlled = True
         self.game.set_player('player')
+
+    def seed_handler(self, engine, boundsRect):
+        seed_config = self.seed_config
+        bounds = (
+            boundsRect.left, boundsRect.top,
+            boundsRect.right, boundsRect.bottom
+            )
+        cells = []
+        seed_keys = list(seed_config.keys())
+        shuffle(seed_keys)
+        for key in seed_keys:
+            cfg = seed_config[key]
+            perlins = self.perlin.perlin_octave_array(
+                bounds[0], bounds[1], engine.world.chunk_size, engine.world.chunk_size,
+                cfg['freq'], cfg['octaves'], 1.0, 32)
+            for (oy, pees) in enumerate(perlins):
+                for (ox, val) in enumerate(pees):
+                    ix = bounds[0] + ox * 32
+                    iy = bounds[1] + oy * 32
+                    if val > cfg['threshold'] and (cfg['rand_thresh'] == 0 or random() > cfg['rand_thresh']):
+                        cells.append(BlueprintCell(cfg['class'], (engine.textures, (ix, iy))))
+
+        return cells
 
     def run(self):
         self.game.run()
