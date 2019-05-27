b83fd9f507bef8c1074f337bb01bda4d90948e26
Derek Arnold <derek@derekarnold.net>
2019-05-27 10:36:35 -0500

    enable prefixes for old sheet format

--- build_assets.py
+++ build_assets.py
@@ -25,20 +25,21 @@ def build_assets(incoming_src_dir, output_dir):
         data = json.load(open('%s/%s' % (incoming_src_dir, fn), 'r'))
         im = Image.open('%s/%s.png' % (incoming_src_dir, part))
         out_images = {}
-        if 'structure' in data and \
-            'type' in data['structure'] and \
+
+        other = {}
+        if 'inherit' in data['structure']:
+            other = json.load(open('%s/%s' % (incoming_src_dir, data['structure']['inherit']), 'r'))
+        if 'names' not in data:
+            data['names'] = other['names']
+        prefix = '' if 'prefix' not in data['structure'] else data['structure']['prefix']
+        if 'type' in data['structure'] and \
             data['structure']['type'] == 'sheet':
-            if 'inherit' in data['structure']:
-                other = json.load(open('%s/%s' % (incoming_src_dir, data['structure']['inherit']), 'r'))
-                if 'stride' not in data['structure']:
-                    data['structure']['stride'] = other['structure']['stride']
-                if 'size' not in data['structure']:
-                    data['structure']['size'] = other['structure']['size']
-                if 'names' not in data:
-                    data['names'] = other['names']
+            if 'stride' not in data['structure']:
+                data['structure']['stride'] = other['structure']['stride']
+            if 'size' not in data['structure']:
+                data['structure']['size'] = other['structure']['size']
             stride = data['structure']['stride']
             size = data['structure']['size']
-            prefix = data['structure']['prefix']
             for i, name in enumerate(data['names']):
                 position = [(i % stride) * size, int(i / stride) * size]
                 nname = '%s_%s' % (prefix, name)
@@ -47,6 +48,7 @@ def build_assets(incoming_src_dir, output_dir):
             for name in data['names']:
                 position = data['names'][name]['position']
                 size = data['names'][name]['size']
-                cropit(im, name, position, size)
+                nname = '%s_%s' % (prefix, name)
+                cropit(im, nname, position, size)
         copyfile('%s/%s' % (incoming_src_dir, fn), '%s/%s' % (output_dir, fn))
 
